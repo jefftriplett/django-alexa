5f7b29850c64793bd4b79ee673ea433c7bdff296
Bill Durr <durrw@fastmail.fm>
2017-03-29 15:40:22 -0400

    remove pyversion

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from sys import version_info
 def open_file(fname):
     return open(path.join(path.dirname(__file__), fname))
 
-setup_requires = ['pbr', 'pyversion']  # TODO: unable to get pyversion working with python 3
+setup_requires = ['pbr']  # TODO: unable to get pyversion working with python 3
 if version_info[0] == 3:
     setup_requires = ['pbr']
 
@@ -14,6 +14,5 @@ setup(
     license='MIT',
     setup_requires=setup_requires,
     pbr=True,
-    auto_version="PBR",
     install_requires=open(path.join(path.dirname(__file__), 'requirements.txt')).readlines(),
 )
